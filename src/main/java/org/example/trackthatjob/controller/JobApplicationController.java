d5858b75c399f733c62388afb8cd2f7b06ce5da3
Kasztel <foltyn.filip@outlook.com>
2024-11-05 11:47:27 +0100

    cross origin annotation added

--- src/main/java/org/example/trackthatjob/controller/JobApplicationController.java
+++ src/main/java/org/example/trackthatjob/controller/JobApplicationController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/applications")
+@CrossOrigin(origins = "http://localhost:3000")
 public class JobApplicationController {
 
     private final JobApplicationService applicationService;
